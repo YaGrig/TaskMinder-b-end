3c9f1b44c7fc259dc7732a2986a795b784ed65ef
YaGrig <yashagrigorev@gmail.com>
2024-08-21 19:16:01 +0300

    TaskMinder-b-end Project

--- src/main/java/com/example/demo/config/ConditionalAuthFilter.java
+++ src/main/java/com/example/demo/config/ConditionalAuthFilter.java
@@ -1,35 +1,35 @@
-package com.example.demo.config;
-
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
-
-import java.io.IOException;
-
-@Component
-@RequiredArgsConstructor
-public class ConditionalAuthFilter extends OncePerRequestFilter {
-
-    private final JWTAuthenticationFilter jwtAuthenticationFilter;
-
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String uri = request.getRequestURI();
-        if ("/graphql".equals(uri)) {
-            filterChain.doFilter(request, response);
-        } else if (isWhiteListed(uri)) {
-            filterChain.doFilter(request, response);
-        } else {
-//            jwtAuthenticationFilter.doFilterInternal(request, response, filterChain);
-            filterChain.doFilter(request, response);
-        }
-    }
-    private boolean isWhiteListed(String uri) {
-        return uri.startsWith("/api/v1/auth/") || uri.startsWith("/token/refresh");
-    }
-
-}
+//package com.example.demo.config;
+//
+//import jakarta.servlet.FilterChain;
+//import jakarta.servlet.ServletException;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.filter.OncePerRequestFilter;
+//
+//import java.io.IOException;
+//
+//@Component
+//@RequiredArgsConstructor
+//public class ConditionalAuthFilter extends OncePerRequestFilter {
+//
+//    private final JWTAuthenticationFilter jwtAuthenticationFilter;
+//
+//    @Override
+//    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+//        String uri = request.getRequestURI();
+//        if ("/graphql".equals(uri)) {
+//            filterChain.doFilter(request, response);
+//        } else if (isWhiteListed(uri)) {
+//            filterChain.doFilter(request, response);
+//        } else {
+////            jwtAuthenticationFilter.doFilterInternal(request, response, filterChain);
+//            filterChain.doFilter(request, response);
+//        }
+//    }
+//    private boolean isWhiteListed(String uri) {
+//        return uri.startsWith("/api/v1/auth/") || uri.startsWith("/token/refresh");
+//    }
+//
+//}
